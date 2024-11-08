d37621b89a280bf935ce931c69e9a5fd1ab1be87
filio <ftitopoulos@gmail.com>
2024-11-08 18:38:09 +0200

    web

---
+++ todo-app/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Task
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form.get('title')
+    new_task = Task(title=title)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit_task(task_id):
+    task = Task.query.get(task_id)
+    if request.method == 'POST':
+        task.title = request.form.get('title')
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', task=task)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
